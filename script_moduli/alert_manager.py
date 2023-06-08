f3a25afe11767e3645b8824240ce2423102622d8
Mario Carbone <mario.carbone10@gmail.com>
2023-06-08 07:04:35 +0200

    Test with Pseudo-Modules

---
+++ script_moduli/alert_manager.py
@@ -0,0 +1,10 @@
+import json
+
+class AlertManager:
+
+    def process_alert(self, alert):
+        # Processa l'alert in formato JSON e prende decisioni in base ai valori di distanza
+        # Implementa la logica per determinare se arrestare il veicolo, rallentarlo, ecc.
+
+    def receive_alerts(self):
+        # Riceve gli alert da qualche sorgente (es. MQTT) e li processa utilizzando il metodo process_alert
