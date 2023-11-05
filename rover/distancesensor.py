ceac46b96ed01cab9215e7a2e71ee7202c5d4301
Mario Carbone <mario.carbone10@gmail.com>
2023-11-05 19:56:29 +0100

    fix

--- rover/distancesensor.py
+++ rover/distancesensor.py
@@ -3,7 +3,7 @@ from gpiozero import DistanceSensor
 
 ultrasonic = DistanceSensor(echo=17, trigger=4)
 
-def get_distance(distance_value)::
+def get_distance(distance_value):
     with distance_value.get_lock():
         distance_value.value = round(ultrasonic.distance * 100, 2)
 
