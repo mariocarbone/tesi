8671dd7d4d7b08716f08a133c8c656c5e6fd6464
Mario Carbone <mario.carbone10@gmail.com>
2023-11-05 19:03:15 +0100

    no queue

--- rover/distancesensor.py
+++ rover/distancesensor.py
@@ -1,6 +1,6 @@
 from gpiozero import DistanceSensor
 
-ultrasonic = DistanceSensor(echo=17, trigger=4)
+ultrasonic = DistanceSensor(echo=17, trigger=4, queue_len=1)
 
 def get_distance():
     return round(ultrasonic.distance*100,2)
