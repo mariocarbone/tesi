8a271c224056f1877a23a0af1af855fdef475873
Mario Carbone <mario.carbone10@gmail.com>
2023-11-05 19:08:54 +0100

    fix test

--- rover/distancesensor.py
+++ rover/distancesensor.py
@@ -3,3 +3,8 @@ from gpiozero import DistanceSensor
 ultrasonic = DistanceSensor(echo=17, trigger=4)
 def get_distance():
     return round(ultrasonic.distance*100,2)
+
+
+if __name__ == "__main__":
+    while True:
+        print(get_distance())
