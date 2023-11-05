d3046c8a469abd81830cef4b894c47e6bcc715a6
Mario Carbone <mario.carbone10@gmail.com>
2023-11-05 17:56:12 +0100

    callback ultrasonic

--- rover/ultrasonic.py
+++ rover/ultrasonic.py
@@ -1,32 +1,39 @@
 import RPi.GPIO as GPIO
 import time
- 
+
+# GPIO pin configuration
 GPIO.setmode(GPIO.BOARD)
- 
 GPIO_TRIGGER = 7
 GPIO_ECHO = 11
 
+# Initialize GPIO pins
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+GPIO.setup(GPIO_ECHO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+start_time = 0
+stop_time = 0
 distance = 0
- 
-def distance():
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
- 
-    start_time = time.time()
-    stop_time = time.time()
- 
-    while GPIO.input(GPIO_ECHO) == 0:
+
+# Callback function for echo pin
+def echo_callback(channel):
+    global start_time, stop_time, distance
+
+    if GPIO.input(GPIO_ECHO):
         start_time = time.time()
- 
-    while GPIO.input(GPIO_ECHO) == 1:
+    else:
         stop_time = time.time()
- 
-    time_elapsed = stop_time - start_time
-    distance = (time_elapsed * 34300) / 2
- 
-    return distance
- 
+        time_elapsed = stop_time - start_time
+        distance = (time_elapsed * 34300) / 2
+
+# Register the callback function for the falling edge of the echo signal
+GPIO.add_event_detect(GPIO_ECHO, GPIO.FALLING, callback=echo_callback)
+
+try:
+    while True:
+        # Your main loop code here
+        print("Distance:", distance, "cm")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Measurement stopped")
+    GPIO.cleanup()
