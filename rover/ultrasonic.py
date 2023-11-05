3a218e17c27e594f5937d9b3c9824b632aa3f990
Mario Carbone <mario.carbone10@gmail.com>
2023-11-05 18:22:16 +0100

    no trigger

--- rover/ultrasonic.py
+++ rover/ultrasonic.py
@@ -3,7 +3,6 @@ import time
 
 # GPIO pin configuration
 GPIO.setmode(GPIO.BOARD)
-GPIO_TRIGGER = 7
 GPIO_ECHO = 11
 
 # Variabile globale per la distanza misurata
@@ -12,27 +11,17 @@ distance_value = 0.0
 # Funzione per gestire l'evento GPIO
 def distance_callback(channel):
     global distance_value
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-
-    start_time = time.time()
-    stop_time = time.time()
-
-    while GPIO.input(GPIO_ECHO) == 0:
+    if GPIO.input(channel) == GPIO.HIGH:
         start_time = time.time()
-
-    while GPIO.input(GPIO_ECHO) == 1:
+    else:
         stop_time = time.time()
-
-    time_elapsed = stop_time - start_time
-    distance = (time_elapsed * 34300) / 2
-    distance_value = distance
+        time_elapsed = stop_time - start_time
+        distance = (time_elapsed * 34300) / 2
+        distance_value = distance
 
 # Imposta il callback per l'evento GPIO
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
-GPIO.add_event_detect(GPIO_ECHO, GPIO.FALLING, callback=distance_callback)
+GPIO.add_event_detect(GPIO_ECHO, GPIO.BOTH, callback=distance_callback)
 
 # Funzione per ottenere la distanza misurata
 def get_distance():
