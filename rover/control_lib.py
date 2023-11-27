3ce8a929830cc4ecb24b69a7bb80acca3c26c168
Mario Carbone <mario.carbone10@gmail.com>
2023-11-27 14:45:46 +0100

    fix alert only when 1

--- rover/control_lib.py
+++ rover/control_lib.py
@@ -47,7 +47,8 @@ class Vehicle_Control():
 			if isinstance(response, dict):
 				self.status.update(response)
 				if self.status.get("object_in_front") and callable(self.object_in_front_callback):
-					self.object_in_front_callback()
+					if(self.status["object_in_front"]) == 1:
+						self.object_in_front_callback()
 			else:
 				print("Lo stato di Arduino non è un dizionario valido.")
 
